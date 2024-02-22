110d6e677e7627060f0de5ba5838421e8ad187af
10Torsions <github@10torsions.com>
2024-02-22 18:04:44 +0100

    Updated interface

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Button from '@mui/material/Button';
-
+import { LinkProps } from 'react-router-dom';
 interface ButtonUsageProps {
+  to: LinkProps['to'];  // Using 'to' prop from LinkProps
   text: string;
+  component: React.ElementType;  // Defining the type for the 'component' prop
 }
 
 const ButtonUsage: React.FC<ButtonUsageProps> = ({text}) => {
-  return <Button variant="contained" className='btnChoiceRole'>{text}</Button>;
+  return <Button variant="contained" className='btnChoice'>{text}</Button>;
 }
 
 export default ButtonUsage;
