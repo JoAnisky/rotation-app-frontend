00d22d9b1ded4937013ecf31fc653c00527b5735
10Torsions <github@10torsions.com>
2024-02-28 15:45:23 +0100

    Removed Roleschoices component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,16 @@
-import Roleschoice from "../components/Roleschoice";
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-    return (
-      <div className="index">
-        <h1>Home</h1>
-        <h1>Bienvenue !</h1>
-          <p>Quel rôle avez-vous pour cette insolite activité ?</p>
-        
-        <Roleschoice />
-      </div>
-    );
-  };
-  
-  export default Home;
+  return (
+    <div className="index">
+      <h1>Bienvenue !</h1>
+      <p>Quel rôle avez-vous pour cette insolite activité ?</p>
+      <Button component={Link} to="/participant"> Participant</Button>
+      <Button component={Link} to="/animateur"> Animateur</Button>
+      <Button component={Link} to="/gamemaster"> Maitre du jeu</Button>
+    </div>
+  );
+};
+
+export default Home;
