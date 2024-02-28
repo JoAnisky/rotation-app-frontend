8b2da25fdd97d707afb9a51269648f9eb9c21629
10Torsions <github@10torsions.com>
2024-02-28 16:34:23 +0100

    Updated navbar icons

--- src/components/NavbarDown.tsx
+++ src/components/NavbarDown.tsx
@@ -1,11 +1,8 @@
 import * as React from 'react';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import FolderIcon from '@mui/icons-material/Folder';
-import RestoreIcon from '@mui/icons-material/Restore';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import MapOutlinedIcon from '@mui/icons-material/MapOutlined';
  const NavbarDown = () => {
   const [value, setValue] = React.useState('recents');
 
@@ -16,21 +13,11 @@ import LocationOnIcon from '@mui/icons-material/LocationOn';
   return (
     <BottomNavigation sx={{ width: 500 }} value={value} onChange={handleChange}>
       <BottomNavigationAction
-        label="Recents"
-        value="recents"
-        icon={<RestoreIcon />}
+        label="Stand"
+        value="stand"
+        icon={<StorefrontIcon />}
       />
-      <BottomNavigationAction
-        label="Favorites"
-        value="favorites"
-        icon={<FavoriteIcon />}
-      />
-      <BottomNavigationAction
-        label="Nearby"
-        value="nearby"
-        icon={<LocationOnIcon />}
-      />
-      <BottomNavigationAction label="Folder" value="folder" icon={<FolderIcon />} />
+      <BottomNavigationAction label="Vue générale" value="vue générale" icon={<MapOutlinedIcon />} />
     </BottomNavigation>
   );
 }
