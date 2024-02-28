be07b28a58a9c359b535d18435e285f826d79000
10Torsions <github@10torsions.com>
2024-02-28 14:41:55 +0100

    Removed unused Div et deleted components

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
 //import Button from "./components/Button";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
+import Home from "./pages/Home";
 import Participant from "./pages/Participant";
 import Animator from "./pages/Animator";
 import Gamemaster from "./pages/Gamemaster";
-import Roleschoice from "./components/Roleschoice";
 
 const App = () => {
   return (
-    <div className="index">
-      <div className="_content">
-        <div className="_welcome">
-          <h1>Bienvenue !</h1>
-          <p>Quel rôle avez-vous pour cette insolite activité ?</p>
-        </div>
-
-        <Router>
-          <Roleschoice />
-          <Routes>
-            <Route path="/participant" element={<Participant/>} />
-            <Route path="/animateur" element={<Animator />} />
-            <Route path="/gamemaster" element={<Gamemaster />} />
-          </Routes>
-        </Router>
-        {/* <div className="roles-choice">
-          <Button component={Link} to="/path"text="Participant"></Button>
-          <Button component={Link} to="/path" text="Animateur"></Button>
-          <Button component={Link} to="/path" text="Maître du jeu"></Button>
-        </div> */}
-      </div>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/participant" element={<Participant />} />
+        <Route path="/animateur" element={<Animator />} />
+        <Route path="/gamemaster" element={<Gamemaster />} />
+      </Routes>
+    </>
   );
 };
 
