3ec223a0fd8895bbe2fe83fad390dfbbc103601d
10Torsions <github@10torsions.com>
2024-02-28 17:25:20 +0100

    Created Timer component

---
+++ src/components/Timer.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+
+const Timer: React.FC = () => {
+  return (
+    <Box className="timer-container">
+      <div>
+        <p>Equipe NOM</p>
+        <p>Stand NOM</p>
+      </div>
+    </Box>
+  );
+};
+
+export default Timer;
