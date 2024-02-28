9d6bc965ddc52a993ec82f86b62e734eba1d6c79
10Torsions <github@10torsions.com>
2024-02-28 16:25:33 +0100

    Added box layouts with SCSS styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,29 @@
-import Button from '@mui/material/Button';
-import { Link } from 'react-router-dom';
+import { Box } from "@mui/material";
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div className="index">
-      <h1>Bienvenue !</h1>
-      <p>Quel rôle avez-vous pour cette insolite activité ?</p>
-      <Button component={Link} to="/participant"> Participant</Button>
-      <Button component={Link} to="/animateur"> Animateur</Button>
-      <Button component={Link} to="/gamemaster"> Maitre du jeu</Button>
+      <Box className="_welcome">
+        <h1>Bienvenue !</h1>
+        <p>Quel rôle avez-vous pour cette insolite activité ?</p>
+      </Box>
+
+      <Box className="roles-choice">
+        <Button className="btnChoiceRole" component={Link} to="/participant">
+          {" "}
+          Participant
+        </Button>
+        <Button className="btnChoiceRole" component={Link} to="/animateur">
+          {" "}
+          Animateur
+        </Button>
+        <Button className="btnChoiceRole" component={Link} to="/gamemaster">
+          {" "}
+          Maitre du jeu
+        </Button>
+      </Box>
     </div>
   );
 };
