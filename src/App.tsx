8c30222eb5b093f84467a2f3acb074b3954e88f5
10Torsions <github@10torsions.com>
2024-02-29 13:46:55 +0100

    Removed routes and Router component add

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
 //import Button from "./components/Button";
-import { Routes, Route } from "react-router-dom";
-
-import Home from "./pages/Home";
-import Participant from "./pages/Participant";
-import Animator from "./pages/Animator";
-import Gamemaster from "./pages/Gamemaster";
+import { BrowserRouter } from 'react-router-dom';
+import Router from './pages/router';
 
 const App: React.FC = () => {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/participant" element={<Participant />} />
-        <Route path="/animateur" element={<Animator />} />
-        <Route path="/gamemaster" element={<Gamemaster />} />
-      </Routes>
-    </>
+    <BrowserRouter>
+      <Router/>
+    </BrowserRouter>
   );
 };
 
