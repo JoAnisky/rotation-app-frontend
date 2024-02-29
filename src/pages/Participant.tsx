4b86c5f91956f6a4fda314f2dc3f4772ce404410
10Torsions <github@10torsions.com>
2024-02-29 13:28:02 +0100

    Imported Status and Timer Component

--- src/pages/Participant.tsx
+++ src/pages/Participant.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Layout from "../components/Layout";
+import Status from "../components/Status";
+import Timer from "../components/Timer";
+import { Box } from "@mui/material";
+
 
 const Participant: React.FC = () => {
   return (
     <Layout role="Participant">
-      <h1>Participant</h1>
+      <Status/>
+      <Timer/>
+      <p>A la fin du temps, se rendre :</p>
+      <Box>Stand N°</Box>
     </Layout>
   );
 };
