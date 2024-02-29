7f8ba0e463aed4fe87e5778d1a3b482a7af6dbb7
10Torsions <github@10torsions.com>
2024-02-29 13:47:17 +0100

    Removed Browser router (deplaced in App)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './assets/sass/styles.scss'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
+  </React.StrictMode>
 );
