2798d4f42025d7031c7bea33a4603927c91efb22
10Torsions <github@10torsions.com>
2024-02-29 16:11:53 +0100

    Added BasicSelect (Select) component

--- src/pages/Animator.tsx
+++ src/pages/Animator.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import PageContainer from "../layouts/PageContainer";
+import { Button } from "@mui/material";
+import BasicSelect from "../components/BasicSelect";
+
 
 const Animator: React.FC = () => {
+
   return (
     <PageContainer role="Animateur">
-      <h1>Animateur</h1>
+      <h2>Animateur</h2>
+      <BasicSelect label="Qui suis-je ?" idAttribute="who"/>
+      <h2>Stand</h2>
+      <BasicSelect label="Quel stand ?" idAttribute="why"/>
+      <Button variant="contained" sx={{ minWidth: 300 }}>C'est parti !</Button>
     </PageContainer>
   );
 };
