abb927bbc783cd8598015a66b0d877d0a0eb515e
10Torsions <github@10torsions.com>
2024-02-29 17:38:04 +0100

    Created stopwatch component

---
+++ src/components/Timer/StopWatch.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import Timer from "./Timer";
+import ControlButtons from "./ControlButtons";
+import { Box } from "@mui/material";
+
+function StopWatch() {
+  const [isActive, setIsActive] = useState(false);
+  const [isPaused, setIsPaused] = useState(true);
+  const [time, setTime] = useState(0);
+
+  React.useEffect(() => {
+    let interval: number = 0;
+
+    if (isActive && isPaused === false) {
+      interval = setInterval(() => {
+        setTime((time) => time + 10);
+      }, 10);
+    } else {
+      clearInterval(interval);
+    }
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isActive, isPaused]);
+
+  const handleStart = () => {
+    setIsActive(true);
+    setIsPaused(false);
+  };
+
+  const handlePauseResume = () => {
+    setIsPaused(!isPaused);
+  };
+
+  const handleReset = () => {
+    setIsActive(false);
+    setTime(0);
+  };
+
+  return (
+    <Box>
+      <Timer time={time} />
+      <ControlButtons
+        active={isActive}
+        isPaused={isPaused}
+        handleStart={handleStart}
+        handlePauseResume={handlePauseResume}
+        handleReset={handleReset}
+      />
+    </Box>
+  );
+}
+
+export default StopWatch;
