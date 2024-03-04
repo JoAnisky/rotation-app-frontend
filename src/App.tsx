ef73a432d3cc29904fe2866c2176ebb61f029582
JoAnisky <anisky5@gmail.com>
2024-03-04 12:04:30 +0100

    Wrapped Time context

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 //import Button from "./components/Button";
-import { BrowserRouter } from 'react-router-dom';
-import Router from './routes/router';
+import { BrowserRouter } from "react-router-dom";
+import Router from "./routes/router";
+import TimeProvider from "./context/TimeProvider";
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Router/>
-    </BrowserRouter>
+    <TimeProvider>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </TimeProvider>
   );
 };
 
