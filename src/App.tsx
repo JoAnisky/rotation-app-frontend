24e24349832afbd161e100411f229d73792ae773
JoAnisky <anisky5@gmail.com>
2024-03-04 16:59:42 +0100

    Added useEffect for existingStartTime

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 //import Button from "./components/Button";
+import React, {useEffect} from "react";
 import { BrowserRouter } from "react-router-dom";
 import Router from "./routes/router";
 import TimeProvider from "./context/TimeProvider";
+import { useLocalStorage } from "./hooks/useLocalStorage";
 
 const App: React.FC = () => {
+  const { getItem, setItem } = useLocalStorage("app_start_time");
+
+  useEffect(() => {
+    // Check if app_start_time already exists
+    const existingStartTime = getItem();
+    if (!existingStartTime) {
+      const now = Date.now();
+      setItem(now); // Set app_start_time only if it doesn't exist
+    }
+  }, [getItem, setItem]);
+
   return (
     <TimeProvider>
       <BrowserRouter>
