4a71daf0bc817e5c1906fe10dbcd7fcb1f42ddbb
JoAnisky <anisky5@gmail.com>
2024-03-04 17:00:07 +0100

    Added elapsedTime and turnTime

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -7,15 +7,21 @@ import TimeContext from "../../context/TimeContext"; // Ensure the path is corre
 const Timer: React.FC = () => {
 
   // Use useContext hook to access the current context value for TimeContext
-  const { time } = useContext(TimeContext);
+  const { elapsedTime, turnTime } = useContext(TimeContext);
+  // Calculate remaining time in milliseconds, clamping to 0 if negative
+  const remainingTime = Math.max(0, turnTime - elapsedTime);
+  
+ // Convert remaining time to minutes and seconds
+ const minutes = Math.floor((remainingTime / 60000) % 60);
+ const seconds = Math.floor((remainingTime / 1000) % 60);
 
   return (
     <div className="timer">
       <span className="digits">
-        {("0" + Math.floor((time / 60000) % 60)).slice(-2)}:
+        {("0" + minutes).slice(-2)}:
       </span>
       <span className="digits">
-        {("0" + Math.floor((time / 1000) % 60)).slice(-2)}
+        {("0" + seconds).slice(-2)}
       </span>
     </div>
   );
