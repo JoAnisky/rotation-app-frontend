35987d334005deff62c67d6f852c4704b2a19725
JoAnisky <anisky5@gmail.com>
2024-03-05 11:36:32 +0100

    update: simplified API endpoints

--- src/api/activityApi.ts
+++ src/api/activityApi.ts
@@ -1,21 +1,11 @@
 // Défines all Activities routes
-const ACTIVITIES_ENDPOINT = '/api/activities';
+const ACTIVITIES_ENDPOINT = "/api/activity";
 
 export const ACTIVITIES_API = {
+  // List all activities
+  activities: `${ACTIVITIES_ENDPOINT}`,
 
-    // List all activities
-    getAllActivities: `${ACTIVITIES_ENDPOINT}`,
-
-    // Get a single activity by ID (GET method)
-    getActivityById: (activityId: string) => `${ACTIVITIES_ENDPOINT}/details/${activityId}`,
-
-    // Create a new activity (POST method)
-    createActivity: `${ACTIVITIES_ENDPOINT}/create`,
-
-    // Delete an activity by ID (DELETE method)
-    deleteActivity: (activityId: string) => `${ACTIVITIES_ENDPOINT}/delete/${activityId}`,
-
-    // Update an activity by ID (PUT or PATCH method)
-    updateActivity: (activityId: string) => `${ACTIVITIES_ENDPOINT}/update/${activityId}`,
-
+  // Get, Update, or Delete a single activity by ID (GET, PUT/PATCH, DELETE methods)
+  activityById: (activityId: string): string =>
+    `${ACTIVITIES_ENDPOINT}/${activityId}`,
 };
