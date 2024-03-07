43e7ba00124a7f06ef608d4c8e184863433b1e19
JoAnisky <anisky5@gmail.com>
2024-03-07 11:11:49 +0100

    created: activity type for Activity Data

---
+++ src/services/api/activity/activityTypes.ts
@@ -0,0 +1,19 @@
+interface TeamMember {
+  id: number;
+  name: string;
+}
+
+export interface IActivityData {
+  name: string;
+  activity_date: Date | null;
+  activity_start_time: number | null;
+  createdAt: Date;
+  global_duration: number;
+  nb_participants: number;
+  nb_teams: number;
+  rotation_duration: number;
+  stand_duration: number;
+  status: string;
+  team: TeamMember[];
+  // Add other fields as necessary
+}
