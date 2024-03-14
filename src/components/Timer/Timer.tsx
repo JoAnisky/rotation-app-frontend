1ceba875cd0cdd773415843fb59ab8008450fcd0
JoAnisky <anisky5@gmail.com>
2024-03-14 16:46:55 +0100

    useContext from TimeContext

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,17 +1,15 @@
 /**
  * TimeShow component
  */
-import React from "react";
-import { useTime } from "../../context/TimeContext";
+import { useContext } from "react";
+import TimeContext from "../../context/TimeContext";
 
 const Timer: React.FC = () => {
-
   // Use useContext hook to access the current context value for TimeContext
-  // const { turnTime, elapsedTime } = useContext(TimeContext);
-  const { turnTime, elapsedTime } = useTime();
+  const { turnTime, elapsedTime } = useContext(TimeContext);
 
   const diff = turnTime - elapsedTime;
-  
+
   // Convert turnTime (remaining time) to minutes and seconds
   const minutes = Math.floor((diff / 60000) % 60);
   const seconds = Math.floor((diff / 1000) % 60);
@@ -22,12 +20,8 @@ const Timer: React.FC = () => {
         <p>Elapsed Time : {elapsedTime}</p>
         <p>TurnTime : {turnTime}</p>
       </div>
-      <span className="digits">
-        {("0" + minutes).slice(-2)}:
-      </span>
-      <span className="digits">
-        {("0" + seconds).slice(-2)}
-      </span>
+      <span className="digits">{("0" + minutes).slice(-2)}:</span>
+      <span className="digits">{("0" + seconds).slice(-2)}</span>
     </div>
   );
 };
