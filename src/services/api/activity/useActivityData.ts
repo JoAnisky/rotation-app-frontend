c4874be6b03495c606e330320897a6d694331539
JoAnisky <anisky5@gmail.com>
2024-03-14 16:08:19 +0100

    improved : useCallBack for updateActivityData()

--- src/services/api/activity/useActivityData.ts
+++ src/services/api/activity/useActivityData.ts
@@ -1,9 +1,10 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef, useCallback } from "react";
 import { ACTIVITY_API } from "../../../api/routes/activityRoutes";
 import { IActivityData } from "./activityTypes";
 import useFetch from "../../../hooks/useFetch";
 
 const useActivityData = (id: string) => {
+
   const [activityData, setActivityData] = useState<IActivityData | null>(null);
   const [data, loading, error] = useFetch<IActivityData>(
     ACTIVITY_API.activityById(id)
@@ -12,20 +13,21 @@ const useActivityData = (id: string) => {
   // Use useRef to keep track of the current value of activityData without causing re-renders
   const activityDataRef = useRef<IActivityData | null>(activityData);
 
-  useEffect(() => {
-    activityDataRef.current = activityData;
-  }, [activityData]);
-
-  useEffect(() => {
+  // Separate function for state updates with dependency on `data` to avoid unnecessary re-renders
+  const updateActivityData = useCallback(() => {
     if (data) {
       const currentDataString = JSON.stringify(activityDataRef.current);
       const newDataString = JSON.stringify(data);
-      // Only update the state if the new data is different from the current data
       if (newDataString !== currentDataString) {
         setActivityData(data);
       }
     }
-  }, [data]); // Dependency on 'data' ensures this effect runs whenever 'data' changes
+  }, [data]);
+
+  // Use useEffect with dependency on `data` and `updateActivityData`
+  useEffect(() => {
+    updateActivityData();
+  }, [data, updateActivityData]);
 
   return { activityData, loading, error };
 };
