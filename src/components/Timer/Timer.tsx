72acfbe78c26501d8c516a9e1dd19e04bc4cdabb
JoAnisky <anisky5@gmail.com>
2024-03-17 15:54:49 +0100

    simplifed Timer display

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,27 +1,35 @@
 /**
  * TimeShow component
  */
-import { useContext } from "react";
-import TimeContext from "../../context/TimeContext";
+// import { useContext } from "react";
+// import TimeContext from "../../context/TimeContext";
 
-const Timer: React.FC = () => {
+interface TimerProps {
+  time: number; // Define the type of time prop
+}
+
+const Timer: React.FC<TimerProps> = ({ time }) => {
   // Use useContext hook to access the current context value for TimeContext
-  const { turnTime, elapsedTime } = useContext(TimeContext);
+  // const { turnTime, elapsedTime } = useContext(TimeContext);
 
-  const diff = turnTime - elapsedTime;
+  // const diff = turnTime - elapsedTime;
 
   // Convert turnTime (remaining time) to minutes and seconds
-  const minutes = Math.floor((diff / 60000) % 60);
-  const seconds = Math.floor((diff / 1000) % 60);
+  // const minutes = Math.floor((diff / 60000) % 60);
+  // const seconds = Math.floor((diff / 1000) % 60);
 
   return (
     <div className="timer">
-      <div>
+      {/* <div>
         <p>Elapsed Time : {elapsedTime}</p>
         <p>TurnTime : {turnTime}</p>
-      </div>
-      <span className="digits">{("0" + minutes).slice(-2)}:</span>
-      <span className="digits">{("0" + seconds).slice(-2)}</span>
+      </div> */}
+      <span className="digits">
+        {("0" + Math.floor((time / 60000) % 60)).slice(-2)}:
+      </span>
+      <span className="digits">
+        {("0" + Math.floor((time / 1000) % 60)).slice(-2)}
+      </span>
     </div>
   );
 };
