9c6f7d565ec110d97a1dcf48ac722f96203e0670
JoAnisky <anisky5@gmail.com>
2024-03-17 15:55:18 +0100

    added : unique stopwatch component for testing

--- src/pages/Gamemaster.tsx
+++ src/pages/Gamemaster.tsx
@@ -1,14 +1,21 @@
-import React from "react";
 import PageContainer from "../layouts/PageContainer";
-import ControlButtons from "../components/Timer/ControlButtons";
-import Timer from "../components/Timer/Timer";
+// import ControlButtons from "../components/Timer/ControlButtons";
+// import Timer from "../components/Timer/Timer";
+import StopWatch from "../components/Timer/Stopwatch";
+// import { ActivityContext } from "../contexts/ActivityContext";
 
 const Gamemaster: React.FC = () => {
+  
+  // Get ActivityData
+  // const activityData = useContext(ActivityContext);
+  // const { name, status } = activityData;
+
   return (
     <PageContainer role="Maître du jeu">
       <h1>Maître du jeu</h1>
-      <ControlButtons />
-      <Timer />
+      <StopWatch />
+      {/* <ControlButtons />
+      <Timer /> */}
     </PageContainer>
   );
 };
