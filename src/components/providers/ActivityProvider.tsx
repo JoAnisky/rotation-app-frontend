c789a0ba963d788dd3ce50098adb51fa373654b8
JoAnisky <anisky5@gmail.com>
2024-03-23 23:15:53 +0100

    added ActivityData refresh every seconds

--- src/components/providers/ActivityProvider.tsx
+++ src/components/providers/ActivityProvider.tsx
@@ -1,42 +1,53 @@
 import React, { useState, useEffect } from "react";
 import { IActivityData } from "../../types/activityTypes";
 import { ActivityContext } from "../../contexts/ActivityContext";
-import useActivityData from "../../hooks/useActivityData"; // Import your custom hook
+import { ACTIVITY_API } from "../../api/routes/activityRoutes";
+// import useActivityData from "../../hooks/useActivityData";
 
 interface ActivityContextValue extends IActivityData {
   loading: boolean;
-  error: boolean; // Change the type accordingly
+  error: boolean; 
 }
 
 interface Props {
   children: React.ReactNode;
 }
+
 const ActivityProvider: React.FC<Props> = ({ children }) => {
   const [activityData, setActivityData] = useState<IActivityData | null>(null);
   const [loading, setLoading] = useState<boolean>(false); // Initial loading state
   const [error, setError] = useState<boolean>(false); // Initial error state
-  const {
-    activityData: fetchedActivityData,
-    error: fetchError,
-    loading: fetchLoading,
-  } = useActivityData("15");
+  // const {
+  //   activityData: fetchedActivityData,
+  //   error: fetchError,
+  //   loading: fetchLoading,
+  // } = useActivityData("1");
 
-  useEffect(() => {
-    if (fetchedActivityData) {
-    //   console.log(fetchedActivityData);
-      setActivityData(fetchedActivityData);
-    }
-  }, [fetchedActivityData]);
-
-  useEffect(() => {
-    setLoading(fetchLoading); // Update loading state based on fetchLoading from the custom hook
-  }, [fetchLoading]);
-
-  useEffect(() => {
-    if (fetchError) {
-      setError(true);
-    }
-  }, [fetchError]);
+  
+    const fetchData = async () => {
+      try {
+        setLoading(true); // Set loading state to true
+        const response = await fetch(ACTIVITY_API.activityById("1"), {
+          method: "GET",
+        });
+        if (response.ok) {
+          const jsonData = await response.json();
+          setActivityData(jsonData);
+        } else {
+          setError(true); // Set error state if response is not ok
+        }
+      } catch (error) {
+        console.error("Polling error:", error);
+        setError(true); // Set error state on any error during fetching
+      } finally {
+        setLoading(false); // Set loading state to false after fetching (success or failure)
+        setTimeout(fetchData, 1000); // Schedule next fetch after 1 second
+      }
+    };
+  
+    useEffect(() => {
+      fetchData();
+    }, []);
 
   const contextValue: ActivityContextValue | undefined = activityData
     ? { ...activityData, loading, error }
