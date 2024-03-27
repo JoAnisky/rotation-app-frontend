f2f6990eb3f0ce97598e539dd8cf221e119c9711
10Torsions <github@10torsions.com>
2024-03-27 15:52:17 +0100

    added: activity name and activity Data

--- src/pages/Gamemaster.tsx
+++ src/pages/Gamemaster.tsx
@@ -1,19 +1,35 @@
 import PageContainer from "../layouts/PageContainer";
 // import ControlButtons from "../components/Timer/ControlButtons";
 import StopWatch from "../components/Timer/Stopwatch";
-// import { ActivityContext } from "../contexts/ActivityContext";
+import { ActivityContext } from "../contexts/ActivityContext";
+import { useContext, useEffect, useState } from "react";
+import { IActivityData } from "../types/activityTypes";
 
 const Gamemaster: React.FC = () => {
-  
+
+  const [activityData, setActivityData] = useState<IActivityData | null>(null);
+  const [activityName, setActivityName] = useState<string>("")
+
   // Get ActivityData
-  // const activityData = useContext(ActivityContext);
-  // const { name, status } = activityData;
+  const activityDataProvider = useContext(ActivityContext);
+  
+  // Retrieve current activity data
+  useEffect(() => {
+    if (activityDataProvider) {
+      setActivityData(activityDataProvider)
+      setActivityName(activityDataProvider.name)
+    } else {
+      // still loading or null ??
+    }
+  }, [activityDataProvider]);
 
   return (
     <PageContainer role="Maître du jeu">
-      <h1>Maître du jeu</h1>
+      <h3>Gestion de l'activité</h3>
+      <p>Nom :  {activityName}</p>
       {/* <ControlButtons/> */}
       <StopWatch isAdmin={true}/>
+
     </PageContainer>
   );
 };
