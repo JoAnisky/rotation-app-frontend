b316e702dbc2b2d17c688fea2ef5b21909ded403
10Torsions <github@10torsions.com>
2024-04-03 14:16:59 +0200

    integrated: stand component

--- src/pages/Participant.tsx
+++ src/pages/Participant.tsx
@@ -1,25 +1,10 @@
-import React from "react";
-import PagesContainer from "../layouts/PageContainer";
-import Status from "../components/Status";
-import { Box } from "@mui/material";
-import StopWatch from "../components/Timer/Stopwatch";
+import Stand from "./Stand";
+
 
 const Participant: React.FC = () => {
 
   return (
-    <PagesContainer role="Participant">
-      <Status />
-      <Box className="timer-container">
-      <div>
-        <p>Equipe NOM</p>
-        <p>Stand NOM</p>
-      </div>
-      <StopWatch isAdmin={false}/>
-      </Box>
-
-      <p>A la fin du temps, se rendre :</p>
-      <Box>Stand N°</Box>
-    </PagesContainer>
+    <Stand role="Participant" isAdmin={false}/>
   );
 };
 
