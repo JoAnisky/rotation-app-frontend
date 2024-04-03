271a9da269362510ab825740b3370a70f8638ef2
10Torsions <github@10torsions.com>
2024-04-03 14:05:54 +0200

    renamed stand by id method

--- src/api/routes/standRoutes.ts
+++ src/api/routes/standRoutes.ts
@@ -6,6 +6,6 @@ export const STANDS_API = {
   stands: `${STANDS_ENDPOINT}`,
 
   // Get, Update, or Delete a single activity by ID (GET, PUT/PATCH, DELETE methods)
-  animatorById: (standId: string): string =>
+  standById: (standId: string): string =>
     `${STANDS_ENDPOINT}/${standId}`,
 };
