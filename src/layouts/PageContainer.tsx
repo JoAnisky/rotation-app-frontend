d0f25a0288530f8344f9864c2c14e6d19a3cbef2
10Torsions <github@10torsions.com>
2024-04-04 10:39:21 +0200

    added : handleChangeSelection function

--- src/layouts/PageContainer.tsx
+++ src/layouts/PageContainer.tsx
@@ -7,14 +7,17 @@ import NavbarDown from "../components/NavbarDown";
 type PageContainerProps = {
   children: React.ReactNode; // To accept any valid React component or HTML element
   role: string;
+  handleChangeSelection?: () => void;
+  animatorStandSetted: boolean;
 };
 
-const PageContainer: React.FC<PageContainerProps> = ({ children, role }) => {
+const PageContainer: React.FC<PageContainerProps> = ({ children, role, handleChangeSelection, animatorStandSetted }) => {
   return (
     <React.Fragment>
       <CssBaseline />
       <Box height="100vh" display="flex" flexDirection="column">
-        <NavbarUp role={role} />
+        <NavbarUp role={role} handleChangeSelection={() => handleChangeSelection?.()} animatorStandSetted={animatorStandSetted} />
+
         <Box className="content-wrapper">{children}</Box>
         <Box
           display="flex"
--- src/pages/Stand.tsx
+++ src/pages/Stand.tsx
@@ -8,13 +8,25 @@ interface StatusProps {
   role: string;
   isAdmin: boolean;
   standName: string | undefined;
+  handleChangeSelection?: () => void;
+  animatorStandSetted: boolean;
 }
 
-const Stand: React.FC<StatusProps> = ({ role, isAdmin, standName }) => {
+const Stand: React.FC<StatusProps> = ({
+  role,
+  isAdmin,
+  standName,
+  handleChangeSelection,
+  animatorStandSetted,
+}) => {
   return (
-    <PagesContainer role={role}>
+    <PagesContainer
+      role={role}
+      handleChangeSelection={() => handleChangeSelection?.()}
+      animatorStandSetted={animatorStandSetted}
+    >
       <Status />
-        <h2>{standName}</h2>
+      <h2>{standName}</h2>
       <Box className="timer-container">
         <div>
           <p>Equipe NOM</p>
