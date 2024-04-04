7f3834eda4baac7e6ecae6060b766db8a36b05e2
10Torsions <github@10torsions.com>
2024-04-04 17:49:01 +0200

    passed animatorStandSetted to false

--- src/pages/Participant.tsx
+++ src/pages/Participant.tsx
@@ -1,11 +1,7 @@
 import Stand from "./Stand";
 
-
 const Participant: React.FC = () => {
-
-  return (
-    <Stand role="Participant" isAdmin={false} standName=""/>
-  );
+  return <Stand role="Participant" isAdmin={false} standName="" animatorStandSetted={false} />;
 };
 
 export default Participant;
