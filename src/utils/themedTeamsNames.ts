c240a51caf756210c8143d0109b9dc2db8a8d22d
JoAnisky <anisky5@gmail.com>
2024-04-12 23:06:33 +0200

    added themedTeamsNames for dynamic generation

---
+++ src/utils/themedTeamsNames.ts
@@ -0,0 +1,35 @@
+const themedTeamsNames: Record<string, string[]> = {
+    Couleurs: [
+      "Rouge", "Vert", "Bleu", "Jaune", "Violet", "Orange", "Indigo", "Gris", "Noir", "Blanc",
+      "Bordeaux", "Turquoise", "Magenta", "Saumon", "Amande", "Crème", "Doré", "Argenté", "Bronze", "Corail",
+      "Saphir", "Opale", "Topaze", "Olive", "Menthe", "Lavande", "Marron", "Beige", "Cyan", "Fuchsia"
+    ],
+    Légumes: [
+        "Carotte", "Tomate", "Courgette", "Aubergine", "Poivron", "Brocoli", "Chou-fleur", "Asperge", "Épinard", "Salade",
+        "Pomme de terre", "Oignon", "Haricot vert", "Petit pois", "Radis", "Concombre", "Champignon", "Navet", "Céleri", "Artichaut",
+        "Blette", "Fenouil", "Gombo", "Panais", "Patate douce", "Poireau", "Rutabaga", "Topinambour", "Betterave", "Chou kale"
+    ],
+    Fruits: [
+        "Pomme", "Banane", "Orange", "Poire", "Raisin", "Mangue", "Fraise", "Cerise", "Ananas", "Melon",
+        "Pastèque", "Kiwi", "Pêche", "Nectarine", "Abricot", "Prune", "Framboise", "Myrtille", "Citron", "Lime",
+        "Mandarine", "Pamplemousse", "Figue", "Papaye", "Avocat", "Kaki", "Litchi", "Durian", "Grenade", "Coing"
+    ],
+    Pâtisseries: [
+        "Croissant", "Éclair au chocolat", "Tarte aux pommes", "Macaron", "Mille-feuille", "Opéra", "Paris-Brest", "Profiterole", "Madeleine", "Financier",
+        "Cannelé", "Tarte tatin", "Clafoutis", "Pain au chocolat", "Brioche", "Sablé", "Éclair au café", "Tarte au citron", "Fondant au chocolat", "Tarte aux fraises",
+        "Palmier", "Gauffre", "Beignet", "Baba au rhum", "Kouign-amann", "Chouquette", "Tarte normande", "Saint-Honoré", "Charlotte aux fraises", "Quatre-quarts"
+    ],
+    Planètes: [
+      "Mercure", "Vénus", "Terre", "Mars", "Jupiter", "Saturne", "Uranus", "Neptune", "Pluton", "Eris",
+      "Haumea", "Makemake", "Cérès", "Sedna", "Orcus", "Quaoar", "Varuna", "Ixion", "Varda", "Chaos",
+      "Gonggong", "Salacia", "Logos", "Pallas", "Hygiea", "Vesta", "Chariklo", "Huya", "Echeclus", "Teharonhiawako"
+    ],
+    Pays: [
+      "France", "États-Unis", "Canada", "Brésil", "Chine", "Inde", "Russie", "Australie", "Allemagne", "Italie",
+      "Espagne", "Mexique", "Japon", "Corée du Sud", "Argentine", "Chili", "Afrique du Sud", "Nigéria", "Égypte", "Turquie",
+      "Iran", "Indonésie", "Thaïlande", "Suède", "Norvège", "Finlande", "Danemark", "Pologne", "Belgique", "Pays-Bas"
+    ]
+  };
+  
+  export default themedTeamsNames;
+  
