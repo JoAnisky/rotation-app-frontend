46566a35a2ef85b93cdbd2679f4204e41303ebbd
10Torsions <github@10torsions.com>
2024-04-17 10:40:23 +0200

    created generalview component

---
+++ src/pages/GeneralView.tsx
@@ -0,0 +1,14 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+const GeneralView = () => {
+  return (
+    <div>
+      <Typography component="h1" variant="h5" align="center" sx={{ mb: 2 }}>
+        Vue générale de l'activité
+      </Typography>
+    </div>
+  );
+};
+
+export default GeneralView;
