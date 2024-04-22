d07ed43726d8448048702426aa02516e9fdbc549
JoAnisky <anisky5@gmail.com>
2024-04-22 14:56:49 +0200

    created index for api routes export

---
+++ src/routes/api/index.ts
@@ -0,0 +1,7 @@
+import { ACTIVITY_API } from "./activityRoutes";
+import { SCENARIO_API } from "./scenarioRoutes";
+import { ANIMATOR_API } from "./animatorRoutes";
+import { STANDS_API } from "./standRoutes";
+import { STOPWATCH_API } from "./stopwatchRoutes";
+
+export { ACTIVITY_API, SCENARIO_API, ANIMATOR_API, STANDS_API, STOPWATCH_API };
