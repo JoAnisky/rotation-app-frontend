05d7c3bcc3be8c276bb8026d3133cb73cbc0311a
10Torsions <github@10torsions.com>
2024-04-25 17:03:58 +0200

    added default structure to participant

--- src/pages/Participant.tsx
+++ src/pages/Participant.tsx
@@ -1,7 +1,28 @@
+import { Container } from "@mui/material";
 import Stand from "./Stand";
+import useActiveComponent from "@/hooks/useActiveComponent";
+import GeneralView from "./GeneralView";
+import NavbarDown from "@/components/NavbarDown";
+import NavbarUp from "@/components/NavbarUp";
 
 const Participant: React.FC = () => {
-  return <Stand role="Participant" standName="" animatorStandSetted={false} />;
+  const { setActiveComponent, renderActiveComponent } = useActiveComponent({
+    defaultComponent: "Stand",
+    components: {
+      Stand: <Stand />,
+      GeneralVieuw: <GeneralView />
+    }
+  });
+  return (
+    <Container sx={{ display: "flex", flexDirection: "column", height: "100vh", padding: "0" }}>
+      <NavbarUp
+        role={"Participant"}
+        animatorStandSetted={true}
+      />
+      {renderActiveComponent()}
+      <NavbarDown setActiveComponent={setActiveComponent} isAdmin={false} />
+    </Container>
+  );
 };
 
 export default Participant;
