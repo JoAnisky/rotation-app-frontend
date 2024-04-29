3ab47588b79c27c3b0601874e9f4261bc1e38318
10Torsions <github@10torsions.com>
2024-04-29 16:14:42 +0200

    Designed stand and getted props

--- src/pages/Stand.tsx
+++ src/pages/Stand.tsx
@@ -1,39 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Status from "@/components/Status";
-import { Box, Container } from "@mui/material";
+import { Box, Container, Grid, Typography } from "@mui/material";
 import Stopwatch from "@/components/Timer/Stopwatch";
+import { IStand } from "@/types/ActivityInterface";
 
 interface StatusProps {
-  standName?: string | undefined;
+  standInfos: IStand[];
 }
 
-const Stand: React.FC<StatusProps> = ({ standName }) => {
+const Stand: React.FC<StatusProps> = ({ standInfos }) => {
+  const [standName, setStandName] = useState<string | undefined>();
+
+  const [currentTeamName, setCurrentTeamName] = useState<string>("Nom team");
+  const [nextTeamName, setNexTeamName] = useState<string>("Nom team");
+  
+  useEffect(() => {
+
+    if (standInfos.length > 0) {
+      const { name } = standInfos[0];
+      setStandName(name);
+    }
+  }, [standInfos]);
+
   return (
     <Container
       maxWidth="sm"
       sx={{
-        marginTop: 2,
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
+        mt: 2,
         width: "85%",
         p: 2,
         height: "75vh",
-        justifyContent: "center",
-        gap: "10px"
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between"
       }}
     >
+      {/* Timer and names section centered */}
       <Status />
-      <h2>{standName}</h2>
-      <Box className="timer-container">
-        <div>
-          <p>Equipe NOM</p>
-        </div>
-        <Stopwatch />
+      <Box className="timer-container" sx={{ alignSelf: "center", textAlign: "center" }}>
+        <Typography variant="h6" component="h1">
+          {standName || "Pas récupéré le nom du stand"}
+        </Typography>
+        <Box bgcolor="primary.main" color="primary.contrastText" p={1} borderRadius={1}>
+          {currentTeamName}
+        </Box>
+        {/* <Stopwatch /> */}
       </Box>
 
-      <p>A la fin du temps, se rendre :</p>
-      <Box>Stand N°</Box>
+      {/* Footer content aligned at the bottom of the page */}
+      <Grid container spacing={1} direction="column" sx={{ width: "100%", gap:"10px" }}>
+        <Box sx={{ mt: "auto", textAlign: "center" }}>
+          <Typography variant="button">À la fin du temps, votre équipe va :</Typography>
+          <Box bgcolor="text.secondary" color="primary.contrastText" p={1} borderRadius={1}>
+            {standName || "Non spécifié"}
+          </Box>
+          <Typography variant='button' component="span">Équipe suivante :</Typography>
+          <Box bgcolor="text.secondary" color="primary.contrastText" p={1} borderRadius={1}>
+            {nextTeamName || "Non spécifié"}
+          </Box>
+            (Actuellement sur 'nom stand actuel')
+        </Box>
+      </Grid>
     </Container>
   );
 };
