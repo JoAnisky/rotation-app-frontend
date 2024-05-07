46833e87eff7e2fa97704536964232ff780ca873
JoAnisky <anisky5@gmail.com>
2024-05-07 16:25:20 +0200

    updated ScenarioInterface

--- src/types/ScenarioInterface.ts
+++ src/types/ScenarioInterface.ts
@@ -1,6 +1,20 @@
+// Définir une interface pour représenter une équipe
+interface Team {
+  teamId: number;
+  teamName: string;
+}
+
+// Définir une interface pour représenter un stand
+interface Stand {
+  standId: number;
+  standName: string;
+  teams: Team[];
+}
+
+// Définir une interface pour représenter une activité de scénario
 export interface ScenarioActivity {
-    [key: string]: string[]; // Clé dynamique, valeur est un tableau de strings
-  }
+  [key: string]: Stand;
+}
 
 export interface IScenario {
   id: number;
