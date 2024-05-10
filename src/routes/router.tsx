2a25b86e393a7cf24f1ff0c46a8a2606d136c287
JoAnisky <anisky5@gmail.com>
2024-05-10 10:16:17 +0200

    intergated protected route logic

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,10 +2,20 @@ import { ReactElement } from "react";
 import { Route, Routes } from "react-router-dom";
 import { routerType } from "./router.types";
 import pagesData from "./pagesData";
+import PrivateRoute from './PrivateRoute'; // Assurez-vous que le chemin d'importation est correct
 
 const Router = (): ReactElement => {
-  const pageRoutes = pagesData.map(({ path, title, element }: routerType) => {
-    return <Route key={title} path={`/${path}`} element={element} />;
+  const pageRoutes = pagesData.map(({ path, title, element, protected: isProtected }: routerType) => {
+    const routeElement = (
+      <Route key={title} path={`/${path}`} element={element} />
+    );
+
+    // Si la route est protégée, l'envelopper dans PrivateRoute
+    return isProtected ? (
+      <Route key={title} path={`/${path}`} element={<PrivateRoute />}>
+        {routeElement}
+      </Route>
+    ) : routeElement;
   });
 
   return <Routes>{pageRoutes}</Routes>;
