633238cef809841b4168cbcfb5b162837ef501de
JoAnisky <anisky5@gmail.com>
2024-05-10 08:42:16 +0200

    added Status  and BasicSelect to exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,7 @@
 import NavbarDown from "@/components/NavbarDown";
 import NavbarUp from "@/components/NavbarUp";
 import CustomSnackbar from "@/components/CustomSnackbar";
+import Status from "@/components/Status";
+import BasicSelect from "@/components/BasicSelect";
 
-export {NavbarDown, NavbarUp, CustomSnackbar};
+export {NavbarDown, NavbarUp, CustomSnackbar, Status, BasicSelect};
