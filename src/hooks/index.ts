de33bb4d0759a01990fdc885fa3e497383ae95c1
JoAnisky <anisky5@gmail.com>
2024-05-10 11:40:57 +0200

    created index.ts for custom hooks export

---
+++ src/hooks/index.ts
@@ -0,0 +1,19 @@
+import useActiveComponent from "@/hooks/useActiveComponent";
+import { useActivityContext } from "@/hooks/useActivityContext";
+import useActivityData from "@/hooks/useActivityData";
+import { useAuth } from "@/hooks/useAuth";
+import useFetch from "@/hooks/useFetch";
+import { useLocalStorage } from "@/hooks/useLocalStorage";
+import { useTimer } from "@/hooks/useTimer";
+import { useUser } from "@/hooks/useUser";
+
+export {
+  useActiveComponent,
+  useActivityContext,
+  useActivityData,
+  useAuth,
+  useFetch,
+  useLocalStorage,
+  useTimer,
+  useUser
+};
