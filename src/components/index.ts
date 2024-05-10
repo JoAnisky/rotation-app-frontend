c529e11351c2124003f3e0dba8db7d9d8ac4afa9
JoAnisky <anisky5@gmail.com>
2024-05-10 08:40:27 +0200

    created index.ts for components and pages exports

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+import NavbarDown from "@/components/NavbarDown";
+import NavbarUp from "@/components/NavbarUp";
+import CustomSnackbar from "@/components/CustomSnackbar";
+
+export {NavbarDown, NavbarUp, CustomSnackbar};
---
+++ src/pages/index.ts
@@ -0,0 +1,11 @@
+import ActivityChoice from "./ActivityChoice";
+import ActivityCode from "./ActivityCode";
+import Animator from "./Animator";
+import Home from "./Home";
+import Gamemaster from "./Gamemaster";
+import GeneralView from "./GeneralView";
+import Participant from "./Participant";
+import Scenario from "./Scenario";
+import Stand from "./Stand";
+
+export { ActivityChoice, ActivityCode, Animator, Home, Gamemaster, GeneralView, Participant, Scenario, Stand };
