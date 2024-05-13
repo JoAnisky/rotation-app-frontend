1235de77df4fb12de6edeeb1bab8c235dfaf9a89
JoAnisky <anisky5@gmail.com>
2024-05-13 14:47:18 +0200

    changed login route

--- src/routes/api/loginRoutes.ts
+++ src/routes/api/loginRoutes.ts
@@ -1,15 +1,16 @@
 import { API_BASE_URL } from "@/routes/api/apiBaseUrl";
 
-const LOGIN_ENDPOINT = `${API_BASE_URL}/login`;
+const LOGIN_ENDPOINT = `${API_BASE_URL}`;
 
 export const LOGIN_API = {
   /**
    * Gets userID with provided token
    */
-  login: `${LOGIN_ENDPOINT}`,
+  login: `${LOGIN_ENDPOINT}/api/login`,
 
   /**
    * Endpoint to check login credentials and get access token
    */
-  loginCheck: `${LOGIN_ENDPOINT}_check`
+  loginCheck: `${LOGIN_ENDPOINT}/login_check`
+  
 };
