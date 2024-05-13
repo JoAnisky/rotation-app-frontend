7ad06d36ae457028b6e148f555cbd362f2071e30
JoAnisky <anisky5@gmail.com>
2024-05-13 10:03:08 +0200

    added cors config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),  // Redirige tous les imports '@' vers le dossier 'src'
+    }
+  },
+  server: {
+    cors: {
+      origin: '*',
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE '],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+      credentials: true
     }
   }
 });
