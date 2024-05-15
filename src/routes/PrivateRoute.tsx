6c87b072042b3adf690bf43e92c2ac4bb97424e6
JoAnisky <anisky5@gmail.com>
2024-05-15 14:12:07 +0200

    added checkAuthorization

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -2,11 +2,25 @@ import { useAuth } from '@/providers/AuthProvider';
 import {Navigate, Outlet} from 'react-router-dom'
 
 const PrivateRoutes = () => {
-    const auth = useAuth();
-
-    const {authToken} = auth;
-
-    return authToken ? <Outlet/> : <Navigate to='/'/>
-}
+    const { authToken, isAuthenticated, userRole } = useAuth();
+  
+    const checkAuthorization = () => {
+        // Si l'utilisateur a un token et le bon rôle, il est autorisé
+        if (authToken && (userRole === 'ROLE_GAMEMASTER' || userRole === 'ROLE_ADMIN')) {
+          return true;
+        }
+        
+        // Si pas de token mais authentifié via code PIN
+        // et si le rôle est celui d'un participant ou animateur
+        if (isAuthenticated && (userRole === 'ROLE_PARTICIPANT' || userRole === 'ROLE_ANIMATEUR')) {
+          return true;
+        }
+    
+        // Par défaut, pas autorisé
+        return false;
+      };
+  
+    return checkAuthorization() ? <Outlet /> : <Navigate to="/" />;
+  };
 
 export default PrivateRoutes
