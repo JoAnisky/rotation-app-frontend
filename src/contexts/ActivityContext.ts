8dd4640f30fa0fdb1f12b155139603a7e15a7ba7
10Torsions <github@10torsions.com>
2024-06-03 14:06:26 +0200

    set default value to 0

--- src/contexts/ActivityContext.ts
+++ src/contexts/ActivityContext.ts
@@ -1,8 +1,7 @@
 import { createContext } from "react";
 import { IActivityContext } from "../types/ActivityInterface";
 
-
 export const ActivityContext = createContext<IActivityContext>({
-  activityId: null,
+  activityId: 0,
   setActivityData: () => {} // Fournissez une implémentation par défaut qui ne fait rien
 });
