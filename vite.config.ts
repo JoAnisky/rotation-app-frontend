e3dcbe2c54c27a7f7f4ab7cbb6d1bc4198d800a1
JoAnisky <anisky5@gmail.com>
2024-06-15 11:17:59 +0200

    Added vite plugin PWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import mkcert from "vite-plugin-mkcert";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), mkcert()],
+  plugins: [react(), mkcert(), VitePWA({ registerType: "autoUpdate" })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src") // Redirects '@' imports to 'src' folder
