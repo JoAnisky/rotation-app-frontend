8029a1857e73b6cd05fcb4e5830303894fdc2911
JoAnisky <anisky5@gmail.com>
2024-06-16 16:01:19 +0200

    included manifestConfig with TypeScript

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,19 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import mkcert from "vite-plugin-mkcert";
 import { VitePWA } from "vite-plugin-pwa";
+import { manifestForPlugIn } from "./src/manifestConfig";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), mkcert(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    react(),
+    mkcert(),
+    VitePWA({
+      registerType: manifestForPlugIn.registerType,
+      includeAssets: manifestForPlugIn.includeAssets,
+      manifest: manifestForPlugIn.manifest
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src") // Redirects '@' imports to 'src' folder
