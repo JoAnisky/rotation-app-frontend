7a8c01ab15e3b9fdf3493c651992064b18e2735d
JoAnisky <anisky5@gmail.com>
2024-07-12 16:04:11 +0200

    Added useAuthentication hook

--- src/pages/Gamemaster.tsx
+++ src/pages/Gamemaster.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import useActiveComponent from "@/hooks/useActiveComponent";
 import NavbarUp from "@/components/NavbarUp";
 import ActivityForm from "@/components/Activity/Form/ActivityForm";
@@ -6,26 +6,28 @@ import NavbarDown from "@/components/NavbarDown";
 import Scenario from "./Scenario";
 import GeneralView from "./GeneralView";
 import ActivityChoice from "./ActivityChoice";
-
+import { CustomSnackbarMethods } from "@/types/SnackbarTypes";
+import useCheckAuthentication from "@/hooks/useCheckAuthentication";
 
 const Gamemaster: React.FC = () => {
   const [chosenActivityId, setChosenActivityId] = useState<number | string>(0);
+  const snackbarRef = useRef<CustomSnackbarMethods>(null);
+
+  // Utilisation du hook de vérification de l'authentification
+  useCheckAuthentication(snackbarRef);
 
   const { setActiveComponent, renderActiveComponent } = useActiveComponent({
     defaultComponent: "ActivityForm",
     components: {
       ActivityForm: <ActivityForm chosenActivityId={chosenActivityId} />,
-      Scenario: <Scenario chosenActivityId={chosenActivityId}/>,
-      GeneralView: <GeneralView />,
-    },
+      Scenario: <Scenario chosenActivityId={chosenActivityId} />,
+      GeneralView: <GeneralView />
+    }
   });
 
   return (
     <>
-      <NavbarUp
-        role={"Maitre du jeu"}
-        animatorStandSetted={false}
-      />
+      <NavbarUp role={"Maitre du jeu"} animatorStandSetted={false} />
 
       {/* If activity is not yet selected, choice ou create activity */}
       {!chosenActivityId ? (
