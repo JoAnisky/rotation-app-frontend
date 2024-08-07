13bce100a6f3cd19d6e90d383d04f84670d153f7
JoAnisky <anisky5@gmail.com>
2024-08-07 16:15:26 +0200

    added cookie and csrf token

--- src/components/Activity/ActivitySelection.tsx
+++ src/components/Activity/ActivitySelection.tsx
@@ -1,8 +1,9 @@
 import { Autocomplete, TextField } from "@mui/material";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import useFetch from "@/hooks/useFetch";
 import { ACTIVITY_API } from "@/routes/api/";
-
+import { useAuth } from "@/hooks";
+import { UseFetchOptions } from "@/hooks/useFetch";
 interface IActivites {
   id: number;
   name: string;
@@ -12,19 +13,36 @@ interface ActivitySelectionProps {
   onActivitySelect: (activity: IActivites | null) => void;
 }
 
-const ActivitySelection: React.FC<ActivitySelectionProps> = ({
-  onActivitySelect,
-}) => {
+const ActivitySelection: React.FC<ActivitySelectionProps> = ({ onActivitySelect }) => {
+  const auth = useAuth();
+
+  const { csrfToken } = auth;
+
+  // useMemo to memorize headers
+  const headers = useMemo(() => {
+    const h: HeadersInit = {
+      "Content-Type": "application/json"
+    };
+    if (csrfToken) {
+      h["x-xsrf-token"] = csrfToken;
+    }
+    return h;
+  }, [csrfToken]);
+
+  // useMemo to memorize query options
+  const requestOptions: UseFetchOptions = useMemo(() => ({
+    method: "GET",
+    headers: headers,
+    credentials: "include"
+  }), [headers]);
+
   // Stands for user selection fetched from : All Stand entries
-  const [fetchedActivities, activitiesLoading] = useFetch<IActivites[]>(
-    ACTIVITY_API.activities
-  );
+  const [fetchedActivities, activitiesLoading] = useFetch<IActivites[]>(ACTIVITY_API.activities, requestOptions);
   const [activities, setActivities] = useState<IActivites[]>([]);
 
   useEffect(() => {
     if (Array.isArray(fetchedActivities)) {
       setActivities(fetchedActivities);
-      // console.log(fetchedActivities);
     } else {
       setActivities([]);
     }
@@ -41,15 +59,13 @@ const ActivitySelection: React.FC<ActivitySelectionProps> = ({
             {option.name}
           </li>
         )}
-        getOptionLabel={(option) => option.name}
+        getOptionLabel={option => option.name}
         onChange={(_event, value) => onActivitySelect(value)}
         loading={activitiesLoading}
         loadingText="Chargement..."
         noOptionsText="Aucune option"
         sx={{ width: "100%" }}
-        renderInput={(params) => (
-          <TextField {...params} label="Quelle activité insolite ?" />
-        )}
+        renderInput={params => <TextField {...params} label="Quelle activité insolite ?" />}
       />
     </>
   );
